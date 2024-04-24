c722eca9e3592aaac94904aae45f4bd2956eae3f
sayed elhadad <sayedelhadad@mail.com>
2024-04-25 01:37:03 +0200

    Done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-
+'''
+This script is a start file with flask in python
+'''
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
-@app.route('/hello', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+@app.route("/", strict_slashes=False)
+def home():
+    '''
+    A trial method to start with flask
+    '''
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
